51f51a71fc5a62943b3af35ba9e5f8b7ac485cdf
yagop <yago.perezperez@epfl.ch>
2026-06-19 12:11:29 +0200

    Fixed filtering

--- html/script.js
+++ html/script.js
@@ -1890,12 +1890,17 @@ function initSourceFilter(colors) {
     jQuery("#sourceFilter").selectable({
         stop: function () {
             sourcesFilter = [];
-            jQuery(".ui-selected", this).each(function () {
-                const index = jQuery("#sourceFilter li").index(this);
-                if (Array.isArray(sources[index]))
-                    sources[index].forEach(member => { sourcesFilter.push(member); });
-                else
-                    sourcesFilter.push(sources[index]);
+            jQuery(".ui-selected", this).each(function() {
+                var key = jQuery(this).attr('id').replace('source-filter-', '');
+                for (var i = 0; i < sources.length; i++) {
+                    if (Array.isArray(sources[i]) && sources[i][0] === key) {
+                        sources[i].forEach(function(member) { sourcesFilter.push(member); });
+                        break;
+                    } else if (sources[i] === key) {
+                        sourcesFilter.push(sources[i]);
+                        break;
+                    }
+                }
             });
         }
     });
@@ -2208,6 +2213,10 @@ function processFlarmUpdate() {
     });
 
     req.done(function(data) {
+        if (now === 0) {
+            now = new Date().getTime() / 1000;
+            last = now - 1;
+        }
         var aircraft = data.aircraft || data;
         if (!Array.isArray(aircraft)) return;
 
